e43100a10b90d0d5bb6e0ca82c8bb59f6886d70c
YZiengg <ntyv69@gmail.com>
2024-11-18 08:19:05 +0700

    be-qlynthat

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -3,10 +3,10 @@ const ProductService = require('../services/ProductService');
 
 const createProduct = async (req, res) => {
     try {
-        const { name, image, type, price, countInStock,rating,description } = req.body;
+        const { name, price, description, rating, image, type, countInStock } = req.body;
         console.log('req.body', req.body)
-        
-        if (!name || !image || !type || !price || !countInStock || !rating || !description) {
+
+        if (!name || !price || !description || !rating || !image || !type || !countInStock) {
             return res.status(400).json({
                 status: 'ERROR',
                 message: 'Tất cả các trường đều là bắt buộc'
@@ -26,7 +26,7 @@ const updateProduct = async (req, res) => {
     try {
         const productId = req.params.id;
         const data = req.body;
-        
+
         if (!productId) {
             return res.status(400).json({
                 status: 'ERROR',
@@ -64,7 +64,7 @@ const getDetailProduct = async (req, res) => {
 const deleteProduct = async (req, res) => {
     try {
         const productId = req.params.id;
-        
+
         if (!productId) {
             return res.status(400).json({
                 status: 'ERROR',
@@ -83,8 +83,8 @@ const deleteProduct = async (req, res) => {
 
 const getAllProduct = async (req, res) => {
     try {
-        const {limit, page,sort,filter}  = req.query
-        const response = await ProductService.getAllProduct(Number(limit) || 8 , Number(page) || 0, sort, filter);
+        const { limit, page, sort, filter } = req.query
+        const response = await ProductService.getAllProduct(Number(limit) || 8, Number(page) || 0, sort, filter);
         return res.status(200).json(response);
     } catch (e) {
         return res.status(404).json({
@@ -99,5 +99,5 @@ module.exports = {
     getDetailProduct,
     deleteProduct,
     getAllProduct
-   
+
 };
